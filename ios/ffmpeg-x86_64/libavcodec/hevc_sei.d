50aaa805639fce3ec99c3f10e76ba4364b912e0b
loctv <loc.tran@ascendcorp.com>
2018-06-13 14:11:46 +0700

    Init

---
+++ ios/ffmpeg-x86_64/libavcodec/hevc_sei.d
@@ -0,0 +1,14 @@
+libavcodec/hevc_sei.o: libavcodec/hevc_sei.c libavcodec/golomb.h \
+  libavcodec/get_bits.h libavutil/common.h libavutil/attributes.h \
+  libavutil/macros.h libavutil/version.h libavutil/avconfig.h config.h \
+  libavutil/intmath.h libavutil/mem.h libavutil/error.h \
+  libavutil/avutil.h libavutil/rational.h libavutil/mathematics.h \
+  libavutil/intfloat.h libavutil/log.h libavutil/pixfmt.h \
+  libavutil/internal.h libavutil/timer.h libavutil/cpu.h \
+  libavutil/dict.h libavutil/libm.h libavutil/intreadwrite.h \
+  libavutil/bswap.h libavutil/avassert.h libavcodec/mathops.h \
+  libavutil/reverse.h libavcodec/vlc.h libavcodec/put_bits.h \
+  libavcodec/hevc_ps.h libavutil/buffer.h libavcodec/avcodec.h \
+  libavutil/samplefmt.h libavutil/channel_layout.h libavutil/frame.h \
+  libavcodec/version.h libavcodec/hevc.h libavcodec/hevc_sei.h \
+  libavutil/md5.h
