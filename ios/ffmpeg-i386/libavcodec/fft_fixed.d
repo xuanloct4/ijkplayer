dae0bc4ff74e284bea8cfd0602447d6641ad7c57
loctv <loc.tran@ascendcorp.com>
2018-06-13 14:25:04 +0700

    Origin source from https://github.com/Bilibili/ijkplayer.git

--- ios/ffmpeg-i386/libavcodec/fft_fixed.d
+++
@@ -1,9 +0,0 @@
-libavcodec/fft_fixed.o: libavcodec/fft_fixed.c libavcodec/fft_template.c \
-  libavutil/mathematics.h libavutil/attributes.h libavutil/rational.h \
-  libavutil/intfloat.h libavutil/thread.h config.h libavutil/log.h \
-  libavutil/avutil.h libavutil/common.h libavutil/macros.h \
-  libavutil/version.h libavutil/avconfig.h libavutil/intmath.h \
-  libavutil/mem.h libavutil/error.h libavutil/internal.h \
-  libavutil/timer.h libavutil/cpu.h libavutil/dict.h libavutil/pixfmt.h \
-  libavutil/libm.h libavcodec/fft.h libavcodec/fft-internal.h \
-  libavcodec/mathops.h libavutil/reverse.h
